9b5a7ed0aa57bd2f1da376c4ce3cd08981fe818a
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2022-03-17 00:39:48 -0300

    formatt

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,13 +1,9 @@
-export function RepositoryItem({repository}) {
-  return(
-  <li>
-    <strong>{repository.name ?? 'default'}</strong>
+export function RepositoryItem({ repository }) {
+  return (
+    <li>
+      <strong>{repository.name ?? "default"}</strong>
       <p>{repository.description}</p>
-        <a href={repository.link}>
-        Acessar repositório
-        </a>
-  </li>
-  )
+      <a href={repository.link}>Acessar repositório</a>
+    </li>
+  );
 }
-
-
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,23 +1,21 @@
-import { RepositoryItem } from "./RepositoryItem"
+import { RepositoryItem } from "./RepositoryItem";
 
 const repository = {
-    name: "unform",
-    description: "Forms in React",
-    link: "https://github.com/"
-}
+  name: "unform",
+  description: "Forms in React",
+  link: "https://github.com/",
+};
 
-export function RepositoryList(){
-    return (
-        <section className="repository-list">
-            <h1>
-                Lista de repositórios
-            </h1>
-                <ul>
-               <RepositoryItem  repository={repository}/>
-               <RepositoryItem  repository={repository}/>
-               <RepositoryItem  repository={repository}/>
-               <RepositoryItem  repository={repository}/>
-            </ul>
-        </section>
-    )
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+      <ul>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+      </ul>
+    </section>
+  );
 }
